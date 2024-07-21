882bc365ce7e5583447925cb3db23910c0b19bc6
Shailesh Joshi <94797599+shaileshjj368@users.noreply.github.com>
2024-07-21 03:41:50 +0000

    variables learned

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144556
+let accountEmail = "sj614598@gmail.com"
+var accountPassword = "12345"
+accoutCity = "Mumbai"
+let accountState
+
+console.table([accountId,accountEmail, accountPassword, accoutCity, accountState])
+
+/*
+Const value can't be changed.
+For declaring variable always use let and not var because the var keyword was used in previous
+times for declaring the variable but at that time javascript was unaware about the problem of
+scoping. Later the problem was fixed by introducing a new keyword 'let'. 
+Also we can define the variable with no keyword but it is not appropriate way of defining the 
+variable. And only defined variable has undefined value.
+*/
