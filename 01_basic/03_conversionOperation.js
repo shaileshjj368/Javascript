f93a65ec7c800c2455d17bd6e47bb32cc2fe24d7
Shailesh Joshi <shaileshjj368@gmail.com>
2024-07-22 16:56:12 +0530

    Learned datatypes basics

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,38 @@
+let score = "33"
+
+console.log(typeof score)
+
+let Numberscore = Number(score)
+
+console.log(typeof Numberscore)
+
+//"33" => 33
+//"33abc" => NaN(Not a Number)
+// true => 1/ false => 0
+// null => 0
+// undefined => undefined
+
+let isLogged = "Shailesh"
+let booleanisLogged = Boolean(isLogged)
+
+console.log(booleanisLogged)
+
+//1 => true
+//0 => false
+//"" => false
+//"anyword or letter" => true
+
+
+/***************************************************Operations******************************************************************/
+
+let value = 90
+let Negvalue = -value
+console.log(Negvalue)
+
+console.log(1 + 2) //3
+console.log(1 + "2") //12
+console.log("1" + 2) //12
+console.log("1" + 2 + 2) //122
+console.log(1 + 2 + "2") //32
+
+
