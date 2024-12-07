e4e2edae9cf24809b5adedef87d27616cf49dea2
Shailesh Joshi <94797599+shaileshjj368@users.noreply.github.com>
2024-12-07 02:55:22 +0000

    Scope, arrow function and iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,14 @@
+
+// Immediately Invoked Function Expressions (IIFE)
+
+(function one(){  // named IIFE
+    console.log(`DB CONNECTED`);
+})(); // semicolon is not must but id not written then next iife will give error
+//because interpreter does not know where to stop
+
+( (name) => { // unnamed IIFE
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('shailesh')
+
+// the functions are declared such a way to avoid global scope pollution
+
